ffcd50dcc2b85efb8012cf5db98f6cf388b1814d
Mariesnlk <44849051+Mariesnlk@users.noreply.github.com>
2020-10-27 19:38:36 +0200

    added comments in ukraine for ded

--- src/LL.java
+++ src/LL.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
@@ -16,30 +17,36 @@ public class LL {
 
     public static void main(String args[]) throws IOException {
 
-        String text = readFileAsString("src//file1.txt");
+        String text = readFileAsString("src//file1.txt");//отримуємо текст з файлу
         System.out.println("---- INPUT FROM FILE ----");
-        System.out.println(text);
+        System.out.println(text);//виводимо прочитаний текст у консоль
 
+        //переміщаємо текст у масив стрінгів ділячи його на окремі слова через пробіл
+        //сам пробіл як лексема не буде враховуватись
         String[] strArray = text.split("\\s");
 
+        //створюємо зв'язний список, який є динамічний, тому потім легко можна вставляти у будь-яке місце списку
         LinkedList<String> list = new LinkedList<String>();
+        //проходимось по кожному елементу списка і додаємо його в список
         for (String w : strArray) {
             list.add(w);
         }
 
+        //проходимось по кожному елементу зв'язного списка і на непарну позицію вставляємо пробіл,
+        // який був видалений під час розбиття стрінга на лексеми
         for (int i = 1; i < list.size(); i += 2) {
             list.add(i, " ");
         }
 
         System.out.println("---- OUTPUT ----");
-
+        //проходимось по кожному елементу зв'язного списка і порівнюємо
+        //на відповідність приналежності до певного класу лексем
+        //виводимо відповідне повідомлення
         for (int i = 0; i < list.size(); i++) {
             String w = list.get(i);
-            if (w.matches("!") || w.contains("?")) {
+            if (w.matches("[-\\.!?_,]+")) {
                 System.out.println(w + " - роздільник");
-            } else if (w.matches("_")) {
-                System.out.println(w + " - роздільник");
-            } else if (w.matches(" ")) {
+            } else if (w.matches("\\s")) {
                 System.out.println(w + " - роздільник \" \" ");
             } else if (w.matches(EMAIL)) {
                 System.out.println(w + " - e-mail");
@@ -57,17 +64,37 @@ public class LL {
         }
     }
 
-    private static String readFileAsString(String filePath) throws IOException {
+    /**
+     * метод, який зчитує дані з файлу за допомогою буферрідеру та повертає текст
+     *
+     * @param filePath бере як параметр шлях до фойлу
+     * @return повертає прочитаний текст з файлу як стрічку
+     */
+    private static String readFileAsString(String filePath) {
         StringBuffer fileData = new StringBuffer();
-        BufferedReader reader = new BufferedReader(
-                new FileReader(filePath));
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new FileReader(filePath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         char[] buf = new char[1024];
         int numRead = 0;
-        while ((numRead = reader.read(buf)) != -1) {
+        while (true) {
+            try {
+                if ((numRead = reader.read(buf)) == -1) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             String readData = String.valueOf(buf, 0, numRead);
             fileData.append(readData);
         }
-        reader.close();
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return fileData.toString();
     }
 }
