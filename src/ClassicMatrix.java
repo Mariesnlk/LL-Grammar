c980ee9219a0f07d55a31d631b3600f2a8d5d6ec
Mariesnlk <44849051+Mariesnlk@users.noreply.github.com>
2020-12-01 21:00:22 +0200

    added classic multiplication

---
+++ src/ClassicMatrix.java
@@ -0,0 +1,29 @@
+public class ClassicMatrix {
+
+    public static int[][] multiplyMatrix(int[][] matrix1, int[][] matrix2) {
+        int column1 = matrix1[0].length; // matrix1 columns length
+        int rows1 = matrix1.length;    // matrix1 result rows length
+        int column2 = matrix2[0].length; // matrix2 result columns length
+        int rows2 = matrix2.length;    // matrix2 rows length
+        if(column1 != rows2) return null; // matrix multiplication is not possible
+        int[][] resultMatrix = new int[rows1][column2];
+        for(int i = 0; i < rows1; i++) {
+            for(int j = 0; j < column2; j++) {
+                for(int k = 0; k < column1; k++) {
+                    resultMatrix[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        return resultMatrix;
+    }
+
+    public static void displayMatrix(int[][] resultMatrix) {
+        System.out.println("Resulting matrix is: ");
+        for(int[] row : resultMatrix) {
+            for (int column : row) {
+                System.out.print(column + "    ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/MainMatrix.java
@@ -0,0 +1,26 @@
+
+public class MainMatrix {
+
+    public static final int MATRIX_SIZE = 5;
+
+    public static void main(String[] args) {
+
+        int[][] matrix1 = matrixGeneration(MATRIX_SIZE, MATRIX_SIZE);
+        int[][] matrix2 = matrixGeneration(MATRIX_SIZE, MATRIX_SIZE);
+
+        int[][] resultClassic = ClassicMatrix.multiplyMatrix(matrix1, matrix2);
+        ClassicMatrix.displayMatrix(resultClassic);
+
+    }
+
+    public static int[][] matrixGeneration(int rows, int columns) {
+        int[][] result = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                result[i][j] = (int) Math.round((Math.random() * 30) - 15);
+            }
+        }
+        return result;
+    }
+
+}
